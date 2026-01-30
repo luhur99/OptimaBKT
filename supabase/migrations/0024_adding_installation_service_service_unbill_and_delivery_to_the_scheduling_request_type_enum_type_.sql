ec5c9a6b315aa8d564d39209d63ac41c4be6229c
[elmony] <luhur@budikaryateknologi.com>
2026-01-30 11:35:21 +0700

    [elmony] Memperbaiki nilai enum scheduling_request_type - wrote 1 file(s), executed 1 SQL queries

---
+++ supabase/migrations/0024_adding_installation_service_service_unbill_and_delivery_to_the_scheduling_request_type_enum_type_.sql
@@ -0,0 +1,4 @@
+ALTER TYPE scheduling_request_type ADD VALUE 'INSTALLATION';
+ALTER TYPE scheduling_request_type ADD VALUE 'SERVICE';
+ALTER TYPE scheduling_request_type ADD VALUE 'SERVICE_UNBILL';
+ALTER TYPE scheduling_request_type ADD VALUE 'DELIVERY';
